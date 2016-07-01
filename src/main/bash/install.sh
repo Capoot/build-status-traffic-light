6a6577c81897684e95defe48763103d8cd938aa0
Julian Heise <julian.heise@zalando.de>
2016-07-01 16:50:59 +0200

    added install script; added -version functionality to main executable

---
+++ src/main/bash/install.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+NEW_PATH=$(dirname $0)
+NEW_PATH=$(cd $NEW_PATH && pwd )
+
+echo "This will uninstall all previous versions of the tebs-daemon and install the new version from $NEW_PATH instead."
+echo "Do you want to continue? (y/n)"
+
+read continueInstallation
+if [ $continueInstallation != "y" ]
+then
+    echo "Aborted installation"
+    exit 0
+fi
+
+/etc/init.d/tebs-daemon stop
+
+if [ -e "/etc/init.d/tebs-daemon" ]
+then
+
+    path=$(readlink -f /etc/init.d/tebs-daemon)
+    path=$(dirname $path)
+    path=$(cd $path && pwd)
+
+    echo "There is an old installation in $path"
+    echo "Do you want to import jobs and configuration from that installation? (y/n)"
+
+    read importJobs
+    if [ $importJobs == "y" ]
+    then
+
+        FILES="${path}/data/*"
+        DIR=$(pwd)
+        for f in $FILES
+        do
+            BASENAME=$(basename f)
+            echo "copying job file $f to $DIR/data/$BASENAME..."
+            cp $f ${DIR}/data/${BASENAME}
+        done
+
+        echo "Finished importing job files. You may revert to the old installation by running ${path}/install.sh
+        (supported from version 1.1.0)"
+    else
+        echo "Continuing without importing old jobs"
+    fi
+fi
+
+echo "Updating daemon link /etc/init.d/tebs-daemon to point to ${NEW_PATH}/daemon.sh..."
+
+rm /etc/init.d/tebs-daemon
+ln -s ${NEW_PATH}/daemon.sh /etc/init.d/tebs-daemon
+update-rc.d /etc/init.d/tebs-daemon defaults
+/etc/init.d/tebs-daemon start
--- src/main/java/de/zalando/buildstatus/daemon/BuildStatusPoll.java
+++ src/main/java/de/zalando/buildstatus/daemon/BuildStatusPoll.java
@@ -4,15 +4,30 @@ import de.zalando.buildstatus.BuildStatusMonitor;
 import de.zalando.buildstatus.display.ClewareTrafficLightDisplay;
 import de.zalando.buildstatus.http.SimpleHttpClient;
 import de.zalando.buildstatus.job.JobsIO;
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class BuildStatusPoll {
 
     public static void main(String[] args) throws Exception {
+
+        if("-version".equals(args[0])) {
+            printVersion();
+            System.exit(0);
+        }
 
         BuildStatusMonitor monitor = new BuildStatusMonitor(
                 new ClewareTrafficLightDisplay(),
                 new SimpleHttpClient());
 
         monitor.updateDisplay(JobsIO.readJobs(args[0]));
+    }
+
+    private static void printVersion() throws IOException {
+        InputStream in = BuildStatusPoll.class.getResourceAsStream("/version");
+        String version = IOUtils.toString(in);
+        System.out.println(version);
     }
 }
